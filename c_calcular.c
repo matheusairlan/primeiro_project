f2781a4c0cb0f00a25c63fecf8227cb34f203a8b
matheusairlan <matheusbastosconta@gmail.com>
2025-10-23 09:00:32 +0100

    criar ficheiro em c

---
+++ c_calcular.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int pN;
+    int sN;
+
+    printf("Digite o valor do primeiro numero : \n");
+    scanf("%d", &pN);
+
+    printf("Digite o valor do segundo numero : \n");
+    scanf("%d", &sN);
+
+    printf("Primeiro numero  %d , segundo numero: %d", pN, sN);
+    return 0;
+};
